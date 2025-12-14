2a62638cf3a729589baa9ac6e738f67e47e3c680
Chaiarnon <6812100012@mut.ac.th>
2025-12-15 02:42:52 +0700

    LAB4

---
+++ LAB4/LAB404/LAB4การบ้าน.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int id_digits = 12; // เลขท้าย 2 ตัวของรหัสนักศึกษา
+
+    cout << "Multiplication Table of " << id_digits << endl;
+    cout << "-----------------------------" << endl;
+
+    // วนลูปตั้งแต่ 1 ถึง 12
+    for (int i = 1; i <= 12; i++) {
+        cout << id_digits << " x " << i << " = " << (id_digits * i) << endl;
+    }
+
+    return 0;
+}
---
+++ LAB4/LABHOMEWORK-10-8-6-4-2-0/LABHOMEWORK-10-8-6-4-2-0.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "=======Loop-If 10-8-6-4-2-0=======" << endl;
+	for (int i = 10; i >= 0; i--) {
+
+		if (i % 2 == 0) {
+			cout << i << " ";
+		}
+
+
+
+	}
+
+
+
+
+
+
+
+	return 0;
+}
